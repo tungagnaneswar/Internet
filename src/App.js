63c2233a1e3e298b91b13c50281b1ee4c201089f
tungagnaneswar <TUNGAGNANESWAR12@GMAIL.COM>
2024-10-06 00:22:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>React Application</p>
+    <div className="container">
+      
+      <div className="left-container">
+        {/* Replace with the actual image */}
+        <img className="illustration" src="https://app.kodnest.com/v2/login5.png" alt="Illustration" />
+      </div>
+      
+      <div className="right-container">
+      <h1>P&C Internet Service</h1>
+        <div className="form-container">
+          <h3 className="title">Welcome Back, Buddy</h3>
+          <p className="subtitle">Speed is matter when accessing the internet!</p>
+          
+          <input className="input-field" type="email" placeholder="Email Address" />
+          <input className="input-field" type="password" placeholder="Password" />
+          
+          <a className="forgot-password-link" href="#">Forgot your password?</a>
+          
+          <button className="login-button">Log In</button>
+        </div>
+      </div>
     </div>
   );
 }
